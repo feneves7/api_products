bee0620cb29f5a4654ddc8b3972f7225d0b90bb2
Felipe Neves <felipeneves1999@gmail.com>
2019-11-15 11:42:11 -0300

    Primeiro Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+//iniciando o app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+const urlDatabase = process.env.MONGODB_URI || 'mongodb://localhost/nodeapi';
+mongoose.connect(urlDatabase, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}, (err, res) => {
+    if (err) { 
+        console.log ('ERROR connecting to: ' + urlDatabase + '. ' + err);
+    } else {
+        console.log ('Succeeded connected to: ' + urlDatabase);
+    }
+});
+
+
+requireDir('./src/models');
+
+app.use('/api', require('./src/routes'));
+app.listen(3001);
